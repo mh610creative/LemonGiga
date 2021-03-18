0374ec9b4fd1f6cb972f01a43163221b4f32998c
Morgan Harris <nonsensemh1@gmail.com>
2021-03-18 16:45:39 -0700

    added dummy test to db to preview tile view of product reviews

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import *
 
 # Define the home view
 def splash(request):
     return render(request, 'products/dashboard.html')
 
 def products_index(request):
-    return render(request, 'products/index.html')
+    products = Gear.objects.all()
+    return render(request, 'products/index.html', {'products': products})
 
 def profile(request):
     return render(request, 'profile.html')
