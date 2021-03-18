8cc33497da47d7f261e0a09ba902dd812215832e
Morgan Harris <nonsensemh1@gmail.com>
2021-03-18 15:17:03 -0700

    migrated Gear/Products model - 2 models total

--- main_app/models.py
+++ main_app/models.py
@@ -6,3 +6,21 @@ class Person(models.Model):
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Gear(models.Model):
+    CATEGORY = (
+        ('Peripherals', 'Peripherals'),
+        ('Hardware', 'Hardware'),
+        ('Software', 'Software'),
+        ('Miscellaneous', 'Miscellaneous')
+    )
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    
