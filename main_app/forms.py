f381b1e0f78d5ed15271a80aee32fe6683036695
Morgan Harris <nonsensemh1@gmail.com>
2021-03-19 14:39:34 -0700

    created custom user reg form with email, successful form send to db

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,5 +1,7 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User 
+from django import forms
 from .models import *
 
 class CommentForm(ModelForm):
@@ -8,3 +10,8 @@ class CommentForm(ModelForm):
         model = Comment
         fields = ['person', 'gear','comment']
 
+class CreateUserForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from .models import *
-from .forms import CommentForm
+from .forms import *
 
 # Define the home view
 def splash(request):
@@ -41,7 +41,7 @@ def createComment(request):
     return render(request, 'forms/comment_form.html', context)
 
 def register(request):
-    form = UserCreationForm(request.POST)
+    form = CreateUserForm(request.POST)
     if request.method == "POST":
         if form.is_valid():
             form.save()
