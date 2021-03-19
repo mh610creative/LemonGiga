e66532473f8f1f857b96048b7430f787e611685a
Morgan Harris <nonsensemh1@gmail.com>
2021-03-19 07:53:38 -0700

    Friday first commit, built individual detail url and view

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,9 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.splash, name='splash'),
+
     path('profile/', views.profile, name='profile'),
-    path('reviews/', views.products_index, name='index'),
-    path('reviews/person/', views.person, name='person'),
+
+    path('reviews/', views.reviews_index, name='index'),
+    path('reviews/<int:review_id>', views.review_detail, name='detail'),
+
+    path('reviews/user/', views.person, name='person'),
+
     path('create_comment/', views.createComment, name='create_comment'),
+
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -8,30 +8,36 @@ from .forms import CommentForm
 def splash(request):
     return render(request, 'splash.html')
 
-def products_index(request):
-    products = Gear.objects.all()
-    return render(request, 'products/index.html', {'products': products})
+def reviews_index(request):
+    reviews = Gear.objects.all()
+    return render(request, 'reviews/index.html', {'reviews': reviews})
+
+def review_detail(request, review_id):
+    review = Gear.objects.get(id=review_id)
+    return render(request, 'reviews/detail.html', {'review': review })
 
 def profile(request):
     return render(request, 'profile.html')
 
 def person(request):
-    products = Gear.objects.all()
+    reviews = Gear.objects.all()
     comments = Comment.objects.all()
     context = {
-        'products': products,
+        'reviews': reviews,
         'comments': comments,
     }
     
-    return render(request, 'products/person.html', context)
+    return render(request, 'reviews/person.html', context)
 
 def createComment(request):
-    products = Gear.objects.all()
+    reviews = Gear.objects.all()
     comments = Comment.objects.all()
     form = CommentForm()
     context = {
-        'products': products,
+        'reviews': reviews,
         'comments': comments,
         'form': form
     }
     return render(request, 'forms/comment_form.html', context)
+
+
