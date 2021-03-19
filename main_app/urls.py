441e1a3a2fd2e49d17e232bae256d1024992770f
Morgan Harris <nonsensemh1@gmail.com>
2021-03-18 21:41:00 -0700

    periodic commit

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('reviews/', views.products_index, name='index'),
     path('reviews/person/', views.person, name='person'),
-    path('create_comment', views.createComment, name='create_comment'),
+    path('create_comment/', views.createComment, name='create_comment'),
 
 ]
